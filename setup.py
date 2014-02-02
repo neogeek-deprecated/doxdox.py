f7ca17c3ae4c8df049276382c35d4ebb5fd612a7
Neo Geek <neogeek0101@gmail.com>
2014-02-02 01:19:00 -0500

    Updated release version.
    Updated Jinja2 version.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='doxdox',
-    version='0.1alpha',
+    version='0.2alpha',
     description='A simple documentation generator that takes output from dox and builds a Bootstrap based documentation file.',
     author='Scott Doxey',
     url='https://github.com/neogeek/doxdox',
     packages=['doxdox'],
     scripts=['doxdox/doxdox.py'],
     install_requires=[
-        'Jinja2==2.7.1',
+        'Jinja2==2.7.2',
         'Pygments==1.6',
         'sh==1.09'
     ]
